89a67df33e49585dfd11521ef5301436bdaf6fe3
Amey Sunu <ameysunu00719@gmail.com>
2021-02-07 14:48:19 +0300

    CRUD done

--- lib/pages/saved.dart
+++ lib/pages/saved.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'qr.dart';
+
+AsyncSnapshot<DocumentSnapshot> snapshot;
+Stream<QuerySnapshot> newStream;
 
 class Saved extends StatefulWidget {
   @override
@@ -6,6 +12,12 @@ class Saved extends StatefulWidget {
 }
 
 class _SavedState extends State<Saved> {
+  @override
+  void initState() {
+    super.initState();
+    newStream = firestoreInstance.collection('saved').snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +30,63 @@ class _SavedState extends State<Saved> {
       ),
       backgroundColor: Colors.black,
       body: Container(
-        child: Column(
-          children: [],
+        child: Container(
+          child: StreamBuilder(
+            stream: newStream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var totalgroupCount = 0;
+              List<DocumentSnapshot> groupUsers;
+              if (snapshot.hasData) {
+                groupUsers = snapshot.data.documents;
+                totalgroupCount = groupUsers.length;
+                return (Container(
+                  child: ListView.builder(
+                      itemCount: groupUsers.length,
+                      itemBuilder: (context, int index) {
+                        return SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        border: Border.all(color: Colors.white),
+                                        borderRadius:
+                                            BorderRadius.circular(15.0),
+                                      ),
+                                      width:
+                                          MediaQuery.of(context).size.width * 1,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(10.0),
+                                            child: SelectableText(
+                                                groupUsers[index]
+                                                    .data()['title'],
+                                                style: TextStyle(
+                                                    fontFamily: 'Roboto',
+                                                    fontSize: 20,
+                                                    color:
+                                                        HexColor('#000000'))),
+                                          ),
+                                        ],
+                                      )),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ));
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
       ),
     );
